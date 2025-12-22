b9c46331fde13997b2e0f09d129643954c1e25d6
AleksaAleksZakharov <AleksaAleksZakharov@Greenatom.ru>
2025-12-22 17:05:10 +0300

    fix

--- compare.py
+++ compare.py
@@ -10,6 +10,8 @@ average_accuracy_after_quant = 0
 
 
 def get_original_model():
+    global size_before_quant_mb, average_accuracy_before_quant
+
     tokenizer = AutoTokenizer.from_pretrained(
         ORIGINAL_MODEL_NAME, trust_remote_code=True)
     model = AutoModelForCausalLM.from_pretrained(
@@ -21,13 +23,15 @@ def get_original_model():
 
     size_before_quant_mb = get_model_size(model)
     print(f'Количество параметров ДО: {size_before_quant_mb:.2f} mb')
-
+    # Количество параметров ДО: 15622.59 mb
     average_accuracy_before_quant = get_average_accuracy(model, tokenizer)
     print(
         f'Качество на бенчмарке MMLU ДО: {average_accuracy_before_quant:.2f}')
+    # Качество на бенчмарке MMLU ДО: 0.66
 
 
 def get_quant_model():
+    global size_after_quant_mb, average_accuracy_after_quant
     model = AutoModelForCausalLM.from_pretrained(
         QUANT_MODEL_NAME,
         quantization_config=quantization_config,
@@ -53,12 +57,21 @@ def main():
 
     print('ИТОГ')
     print(f'Количество параметров ДО: {size_before_quant_mb:.2f} mb')
+    # Количество параметров ДО: 15622.59 mb
     print(f'Количество параметров ПОСЛЕ: {size_after_quant_mb:.2f} mb')
+    # Количество параметров ПОСЛЕ: 5686.59 mb
     print(
         f'Качество на бенчмарке MMLU ДО: {average_accuracy_before_quant:.2f}')
-    # Качество на бенчмарке MMLU ДО: 0.65
+    # Качество на бенчмарке MMLU ДО: 0.66
     print(
         f'Качество на бенчмарке MMLU ПОСЛЕ: {average_accuracy_after_quant:.2f}')
+    # Качество на бенчмарке MMLU ПОСЛЕ: 0.65
+    Compression_ratio = size_before_quant_mb / size_after_quant_mb
+    Performance_drop = (average_accuracy_before_quant -
+                        average_accuracy_after_quant) / average_accuracy_before_quant
+    Score = Compression_ratio / (1 + Performance_drop)
+    print(
+        f'Score: {Score:.2f}')
 
 
 if __name__ == '__main__':
